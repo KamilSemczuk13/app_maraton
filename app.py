ce7ab0998f055b0a12e66c62169a01c9511960f2
KamilSemczuk13 <szwemcz2005@gmail.com>
2025-10-22 00:26:42 +0200

    chagnges in app.py

--- app.py
+++ app.py
@@ -679,10 +679,6 @@ if st.session_state["page"]=="result_marathon_time":
         user_text=st.session_state["user_text"]
         dict_user=text_to_dict_lang(user_text, TEXT_TO_TEXT)
 
-        st.markdown(f'''public_key:{st.secrets["LANGFUSE_PUBLIC_KEY"]},
-        secret_key={st.secrets["LANGFUSE_SECRET_KEY"]},
-        host={st.secrets["LANGFUSE_HOST"]}''')
-
         if veryfications_of_info(dict_user["sex"], dict_user["time_5km"], dict_user["time_10km"]):
             info=veryfications_of_info_to_error(dict_user["sex"], dict_user["time_5km"], dict_user["time_10km"])
             st.info(info)
